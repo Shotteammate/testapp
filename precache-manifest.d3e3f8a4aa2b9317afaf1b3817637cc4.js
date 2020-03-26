5856eb86936d7fc7221cc76f0cc77dfc48fc9055
shotteammate <shotteammate@gmail.com>
2020-03-26 12:46:30 +0800

    Updates

---
+++ precache-manifest.d3e3f8a4aa2b9317afaf1b3817637cc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee647708aec3c10e85874f5c6715cb0",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "57df14025922b0ca9dd7",
+    "url": "/testapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03b00b3445522da6287c",
+    "url": "/testapp/static/js/2.ddafb193.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testapp/static/js/2.ddafb193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57df14025922b0ca9dd7",
+    "url": "/testapp/static/js/main.fe07beab.chunk.js"
+  },
+  {
+    "revision": "e78fe68a9952c36db1bc",
+    "url": "/testapp/static/js/runtime-main.d9cea74a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
